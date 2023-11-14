6312e368a7177f1e4945d299d8be9f01f12182c9
dad-guild <dad-guild-loot-council@gmail.com>
2023-11-14 01:57:11 -0800

    re-sort csvs after merging, fix rclc items in gargul exports

--- regenerate.bash
+++ regenerate.bash
@@ -12,7 +12,14 @@ mv gargul/*.csv csv/
 
 pushd rclc > /dev/null
 ls -1 . | xargs -P32 -n1 -I{} sh -c 'sort {} -o {}.bak && mv {}.bak {} && ../convert-rclc.py {}'
+for f in `find . -type f -name "*.csv"`; do
+  cat "$f" >> "../csv/$f"
+  rm "$f"
+done
 popd > /dev/null
-mv rclc/*.csv csv/
+
+pushd csv > /dev/null
+ls -1 . | xargs -P32 -n1 -I{} sh -c 'sort {} -o {}.bak && mv {}.bak {}'
+popd > /dev/null
 
 sed -i '' '1s;^;character,itemName,itemID,offspec,date\r\n;' csv/*.csv
